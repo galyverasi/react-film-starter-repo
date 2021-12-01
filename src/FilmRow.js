5217467cf919cbedeba1a5ba433197462018de46
galyverasi <galyverasi@gmail.com>
2021-11-30 20:20:30 -0700

    added poster

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -5,7 +5,7 @@ export default class FilmRow extends Component {
     render() {
         return (
             <div className="film-row">
-                <FilmPoster thisMovie={this.props.film} />
+                <FilmPoster poster={this.props.film.poster_path} alt={this.props.film.title} />
                     <div className="film-summary">
                         <h1>{this.props.film.title}</h1>
                         <p>{this.props.film.release_date.split('-')[0]}</p>
